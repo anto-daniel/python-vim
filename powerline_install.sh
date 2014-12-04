e432f2de3cee3d5026e22f691872ae07345a1f6e
Anto Daniel <anto.daniel@gmail.com>
2014-12-05 01:25:34 +0530

    adding script

---
+++ powerline_install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash 
+sudo apt-get install python gcc wget fontconfig socat -y
+sudo pip install psutil
+sudo pip install mercurial
+sudo pip install pygit2
+sudo pip install pygit
+sudo pip install bzr
+sudo pip install pyuv
+git clone https://github.com/ziberna/i3-py.git
+cd i3-py && python setup.py build && sudo python setup.py install
+pip install --user git+git://github.com/Lokaltog/powerline
+sudo pip install powerline-status
+wget https://github.com/Lokaltog/powerline/raw/develop/font/PowerlineSymbols.otf
+wget https://github.com/Lokaltog/powerline/raw/develop/font/10-powerline-symbols.conf
+mkdir -p ~/.fonts && mv PowerlineSymbols.otf ~/.fonts/
+fc-cache -vf ~/.fonts/
+mkdir -p ~/.config/fontconfig/conf.d/ && mv 10-powerline-symbols.conf ~/.config/fontconfig/conf.d/
+powerline-daemon -q
+cat >> $HOME/.bashrc << EOM
+export PATH=$PATH:$HOME/.local/bin
+. /home/antodaniel/.local/lib/python2.7/site-packages/powerline/bindings/bash/powerline.sh
+EOM
+source ~/.bashrc
