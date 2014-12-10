8bd87f20dd0d4acbefb4ea29a28009ccf8681c51
anto-daniel <anto.daniel@gmail.com>
2014-12-10 21:26:02 +0530

    Update powerline_install.sh

--- powerline_install.sh
+++ powerline_install.sh
@@ -1,15 +1,14 @@
 #!/bin/bash 
 sudo apt-get install python gcc wget fontconfig socat -y
-sudo pip install psutil
-sudo pip install mercurial
-sudo pip install pygit2
-sudo pip install pygit
-sudo pip install bzr
-sudo pip install pyuv
-git clone https://github.com/ziberna/i3-py.git
-cd i3-py && python setup.py build && sudo python setup.py install
+pip install --user psutil
+pip install --user mercurial
+pip install --user pygit2
+pip install --user pygit
+pip install --user bzr
+pip install --user pyuv
+pip install --user i3-py
 pip install --user git+git://github.com/Lokaltog/powerline
-sudo pip install powerline-status
+pip install --user powerline-status
 wget https://github.com/Lokaltog/powerline/raw/develop/font/PowerlineSymbols.otf
 wget https://github.com/Lokaltog/powerline/raw/develop/font/10-powerline-symbols.conf
 mkdir -p ~/.fonts && mv PowerlineSymbols.otf ~/.fonts/
